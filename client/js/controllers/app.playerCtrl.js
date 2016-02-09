a4f4353003ed2e92f381b0fab9a7ef7859cd75ac
Leonardo Alt <leonardoaltt@gmail.com>
2016-02-09 10:01:44 +0100

    play-pause buttons

--- client/js/controllers/app.playerCtrl.js
+++ client/js/controllers/app.playerCtrl.js
@@ -5,13 +5,28 @@ app.controller('playerCtrl', ['$scope', 'socket', function($scope, socket)
 {
     var self = this;
 
-    self.start = function(address_streaming){socket.emit('start_player', address_streaming);}
-    self.pause = function(){socket.emit('pause_player');}
+    self.is_playing = false;
+
+    self.start = function(address_streaming)
+    {
+        socket.emit('start_player', address_streaming);
+        self.is_playing = true;
+    }
+
+    self.pause = function()
+    {
+        socket.emit('pause_player');
+        self.is_playing = false;
+    }
     self.forward = function(){socket.emit('forward_player');}
     self.backward = function(){socket.emit('backward_player');}
     self.volume_up = function(){socket.emit('volume_up');}
     self.volume_down = function(){socket.emit('volume_down');}
-    self.stop = function(){socket.emit('stop_player');}
 
+    self.stop = function()
+    {
+        socket.emit('stop_player');
+        self.is_playing = false;
+    }
 
 }]);
